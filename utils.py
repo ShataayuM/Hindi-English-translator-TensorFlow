fdf9967c905e74b34583d3cf5b83a06045392300
SHATAAYU MOHANTY <150045477+ShataayuM@users.noreply.github.com>
2025-09-01 18:39:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
+import re
+import unicodedata
 
+def preprocess_sentence(sentence):
+    """Cleans and preprocesses a single sentence."""
+    s = str(sentence)
+    s = re.sub(r"([?.!,¿])", r" \1 ", s)
+    s = re.sub(r'[" "]+', " ", s)
+    s = re.sub(r"[^a-zA-Z?.!,¿\u0900-\u097F]+", " ", s)
+    s = s.strip()
+    return s
