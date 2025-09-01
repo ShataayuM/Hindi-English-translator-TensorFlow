60ba99c890b48be2781fbcdf1bbc97e23ddad8b9
SHATAAYU MOHANTY <150045477+ShataayuM@users.noreply.github.com>
2025-09-01 18:40:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,63 @@
+# model.py
 
+import tensorflow as tf
+
+class Encoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+        super(Encoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.lstm = tf.keras.layers.LSTM(self.enc_units,
+                                         return_sequences=True,
+                                         return_state=True,
+                                         recurrent_initializer='glorot_uniform')
+
+    def call(self, x, hidden):
+        x = self.embedding(x)
+        output, state_h, state_c = self.lstm(x, initial_state=hidden)
+        return output, [state_h, state_c]
+
+    def initialize_hidden_state(self):
+        return [tf.zeros((self.batch_sz, self.enc_units)),
+                tf.zeros((self.batch_sz, self.enc_units))]
+
+class BahdanauAttention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, query, values):
+    if len(query.shape) == 1:
+      query = tf.expand_dims(query, 0)
+    query_with_time_axis = tf.expand_dims(query, 1)
+    score = self.V(tf.nn.tanh(
+        self.W1(values) + self.W2(query_with_time_axis)))
+    attention_weights = tf.nn.softmax(score, axis=1)
+    context_vector = attention_weights * values
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+    return context_vector, attention_weights
+
+class Decoder(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, dec_units, batch_sz):
+    super(Decoder, self).__init__()
+    self.batch_sz = batch_sz
+    self.dec_units = dec_units
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.lstm = tf.keras.layers.LSTM(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.fc = tf.keras.layers.Dense(vocab_size)
+    self.attention = BahdanauAttention(self.dec_units)
+
+  def call(self, x, hidden_states, enc_output):
+    context_vector, attention_weights = self.attention(hidden_states[0], enc_output)
+    x = self.embedding(x)
+    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+    output, state_h, state_c = self.lstm(x, initial_state=hidden_states)
+    output = tf.reshape(output, (-1, output.shape[2]))
+    x = self.fc(output)
+    return x, [state_h, state_c], attention_weights
